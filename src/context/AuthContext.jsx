c652a9ba51f0e6c3a451ee30457769fcaf75ebcf
Felipe Paulino <felipe.ribeiro@isalab.com.br>
2022-12-22 17:46:22 -0300

    frontEnd AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { api } from "../services/api";
+
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({children}) =>{
+    const [user, setUser] = useState(null)
+
+    useEffect(()=>{
+        const loadingStoreData = async () =>{
+            const storageUser = localStorage.getItem("user")
+            const storageToken = localStorage.getItem("token")
+    
+            if(storageToken && storageUser) {
+                setUser(storageUser)
+            }
+        }
+        loadingStoreData()
+    },[])
+
+   
+
+    const signIn = async ({ email, password }) => {
+        try {
+          const response = await api.post("/auth", { email, password });
+          if (response.data.error) {
+            alert(response.data.error);
+          } else {
+            setUser(response.data);
+            api.defaults.headers.common[
+              "Authorization"
+            ] = `Bearer ${response.data.token}`;
+    
+            localStorage.setItem("user", JSON.stringify(response.data.user))
+            localStorage.setItem("token", response.data.token)
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      const signOut = () => {
+        window.location.href = "/" 
+        localStorage.clear();
+        setUser(null);
+      };
+
+    return (
+        <AuthContext.Provider value ={{
+            user,
+            signed: !!user,
+            signIn,
+            signOut,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useContext } from "react";
+import { LayoutComponents } from "../../components/LayoutComponents";
+import { api } from "../../services/api";
+import { AuthContext } from "../../context/AuthContext"
+
+export const Home = () => {
+  const {signOut} = useContext(AuthContext)
+  let userEmail = (localStorage.getItem('user') ?? '')
+
+
+  userEmail = userEmail.replace(/[{}"]/g,"");
+
+  return (
+    
+    <LayoutComponents>
+        <div className="home-content">
+          <h1 className="home-title">
+            <span style={{ color: "black", marginLeft: "120px" }}>Bem Vindo</span>
+          </h1>
+          <br />          
+          <p style={{ color: "black", marginLeft: "50px" }} className="home-description">
+            Você esta logado como {userEmail.split(":",3)[2]}
+          </p>
+        </div>
+      <button onClick={signOut} className="login-form-btn-signOut">sair</button>
+    </LayoutComponents>
+  );
+};
